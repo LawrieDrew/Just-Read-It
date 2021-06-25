ab53121e0abdbeb9dd1d5465b67ec7a7c2999abe
LawrieDrew <laurenn.drew@gmail.com>
2021-06-24 21:56:56 -0400

    continued auth in server.js

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
 const express = require ("express");
-
 const mongoose = require("mongoose");
+
+const Mongoosery = require('connect-mongodb-session')(session.Store);
+
 const app = express();
 const PORT = pprocess.env.PORT || 3001;
 
+const sess = {
+    secret: 'so secret, man',
+    cookie: {},
+    resave: false,
+    saveUnittialized: true,
+    store: new Mongoosery({
+        db: mongoose
+    })
+}
+
+app.use(session(sess));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
+
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
 
