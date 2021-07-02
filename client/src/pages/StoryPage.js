c724f6123972808a4db35521873b152e1afef832
Joey <joewoodnyc@gmail.com>
2021-07-01 21:31:59 -0400

    ADDING SOUNDS, has old app.js

--- client/src/pages/StoryPage.js
+++ client/src/pages/StoryPage.js
@@ -7,8 +7,18 @@ import Content from "../components/Content";
 // import ListItem from "../components/ListItem";
 import Col from "../components/Col";
 
+//FONT AWESOME PLAY BUTTON
+//MUST INSTALL THIS DEPENDENCY TO HAVE PLAY BUTTON - joey
+// npm install --save @fortawesome/free-solid-svg-icons
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlay } from '@fortawesome/free-solid-svg-icons';
 
 function StoryPage(props) {
+  var audio1 = document.getElementById('audio1');
+  var audio2 = document.getElementById('audio2');
+  var audio3 = document.getElementById('audio3');
+  const playButton = <FontAwesomeIcon icon={faPlay} />
+
   const [story, setStory] = useState({})
 
     const {id} = useParams()
@@ -34,6 +44,7 @@ function StoryPage(props) {
       window.location.href="/"
     }
 
+
     return (
         <div>
             <h2>{story.title}</h2> 
@@ -42,18 +53,30 @@ function StoryPage(props) {
               <div id="pageOne">
               <h3>{story.page1}</h3>
               <img src={"./images/"+story.page1Keyword+".png"} alt="icon"/>
+        
+              <audio id="audio1" src={"./audio/"+story.page1Keyword+".mp3"}></audio>
+              <button onClick={() => audio1.play()}>{playButton}</button>
+              
               <button onClick={() => increment()}>Next Page</button>
               </div>
 
               <div id="pageTwo" className="hidden">
               <h3 >{story.page2}</h3>
               <img src={"./images/"+story.page2Keyword+".png"} alt="icon"/>
+
+              <audio id="audio2" src={"./audio/"+story.page2Keyword+".mp3"}></audio>
+              <button onClick={() => audio2.play()}>{playButton}</button>
+
               <button onClick={() => increment2()}>Next Page</button>
               </div>
               
               <div id="pageThree" className="hidden">
               <h3 >{story.page3}</h3>
               <img src={"./images/"+story.page3Keyword+".png"} alt="icon"/>
+
+              <audio id="audio3" src={"./audio/"+story.page3Keyword+".mp3"}></audio>
+              <button onClick={() => audio3.play()}>{playButton}</button>
+
               <button onClick={() => increment3()}>Complete Story!</button>
               </div>
             </Content>
--- server.js
+++ server.js
@@ -1,21 +1,14 @@
 const express = require ("express");
 const mongoose = require("mongoose");
 const session = require('express-session');
-
 const MongoDBStore = require('connect-mongodb-session')(session);
-
-const jwt = require('jsonwebtoken');
-
 const app = express();
 const PORT = process.env.PORT || 3005;
 const routes = require("./routes");
-
-
 var store = new MongoDBStore({
     uri: process.env.MONGODB_URI || "mongodb://localhost/JustReadItDB",
     collection: 'mySessions'
   });
-
 app.use(session({
     secret: process.env.SESSION_SECRET || 'This is a secret',
     cookie: {
@@ -25,17 +18,13 @@ app.use(session({
     resave: true,
     saveUninitialized: true
 }));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
 app.use(routes);
-
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/JustReadItDB", { useNewUrlParser: true });
-
 app.listen(PORT, function() {
-    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+    console.log(`:earth_americas:  ==> API Server now listening on PORT ${PORT}!`);
   });
