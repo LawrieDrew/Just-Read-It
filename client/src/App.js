725e857d34b0626e0ac70893b2e1d391461f33c2
Rachel Strong <rachellynnstrong@gmail.com>
2021-07-01 20:14:44 -0400

    adding things

--- client/src/App.js
+++ client/src/App.js
@@ -1,121 +1,34 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
-import Cookies from "js-cookie";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import StoryPage from "./pages/StoryPage";
 
-import Wrapper from "./components/Wrapper";
-import Auth from "./auth";
+// import Wrapper from "./components/Wrapper";
 
 //need to add login/logout routes below
 function App() {
-
-  const [auth, setAuth] = React.useState(false);
-  const readCookie = () => {
-    Cookies.get("user");
-    if (user){
-      setAuth(true);
-    }
-  }
-
-  React.useEffect(() => {
-    readCookie();
-  }, [])
   return (
 
-      <div>
-        <Auth.Provider value={{auth, setAuth}}>
-        <Router>
-          <Routes>
+  
+          <div>
+            <Router>
+            <Switch>
+                <Route exact path="/">
+                  <Home />
+                </Route>
 
-          </Routes>
-       <div>
-            
-       <Switch>
-              <Route exact path="/">
-                 <Home />
-              </Route>
-
-             <Route exact path="/:id">
-            <StoryPage />
-             </Route>
-         </Switch>
+                <Route exact path="/:id">
+                  <StoryPage />
+                </Route>
+             </Switch>
+            </Router>
 
   
-      </div>
- </Router>
- </Auth.Provider>        
+          </div>
+  
+          
 
   );
-
-
-      </div>
-
-  
-}
-
-//login/logout doodads
-const Login = () => {
-  const Auth = React.useContext(Auth)
-  const handleOnClick = () => {
-    Auth.setAuth(true);
-    Cookies.set("user" , "login")
-  }
-  return(
-    <div>
-      <h1>JUST READ IT!</h1>
-      <button onClick={handleOnClick}>Login</button>
-    </div>
-  )
-}
-
-const Home = () => {
-  return(
-    <div>
-      <h1>Welcome!</h1>
-      <button>Logout</button>
-    </div>
-  )
-}
-
-const Routes = () => {
-  const Auth = React.useContext(Auth)
-  return(
-    <Switch>
-      <Route path="/login" component={Login} auth={Auth.auth}/>
-      <Route path="/Home" auth={Auth.auth} component={Home}/>
-    </Switch>
-  )
-}
-
-const ProtectedRoute = ({auth, component:Component,...rest}) => {
-  return(
-    <Route
-    {...rest}
-    render = {() =>!auth? (
-      <Component/>
-    ):
-    (
-      <Redirect to="/login"/>
-    )
-  }
-    />
-  )
-}
-
-const ProtectedLogin = ({auth, component:Component,...rest}) => {
-  return(
-    <Route
-    {...rest}
-    render = {() =>auth? (
-      <Component/>
-    ):
-    (
-      <Redirect to="/Home"/>
-    )
-  }
-    />
-  )
 }
 
 export default App;
