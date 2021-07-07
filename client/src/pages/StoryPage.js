d86850c3102a9ac516ce4090b7e4657dbc57be15
Neil Creveling <neil.creveling@gmail.com>
2021-07-07 12:40:50 -0400

    commit on 'mongoosery'

--- client/src/pages/StoryPage.js
+++ client/src/pages/StoryPage.js
@@ -1,53 +1,103 @@
-import React from "react";
-import Button from "./components/Button";
-import { Container, Row, Col } from "./components/Grid";
-import Content from "./components/Content";
-import Answers from "./components/Answers";
-import Image from "./components/Image";
-import { text } from "express";
+import React, {useEffect, useState} from "react";
+import {useParams} from "react-router-dom";
+import API from "../utils/API";
+import "./style.css";
+import Content from "../components/Content";
+// import List from "../components/List";
+// import ListItem from "../components/ListItem";
+import Col from "../components/Col";
 
-//import components
+
+function StoryPage(props) {
+  const [story, setStory] = useState({})
+
+    const {id} = useParams()
+    useEffect(() => {
+      API.getStory(id)
+        .then(res => setStory(res.data))
+        .catch(err => console.log(err));
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+   
 
 
-function StoryPage() {
+    function increment() {
+      document.getElementById("pageOne").classList.add("hidden");
+      document.getElementById("pageTwo").classList.remove("hidden");
+    }
+    function increment2() {
+      document.getElementById("pageTwo").classList.add("hidden");
+      document.getElementById("pageThree").classList.remove("hidden");
+    }
+    function increment3() {
+      console.log("incremenet user level + 1")
+      window.location.href="/"
+    }
+
+// shot at populating answer buttons with options
+
+    //var chooseOptions1 = function () {
+      //var unique = true;
+      //num = Math.floor(Math.random() * a.length - 3);
+      //option = {story.page1Options}.splice(num,1);
+      //{story.page1Options}.push(option);
+    //}
+
+
+
     return (
-        <div>
-            <Container>
-                <Col>
-                    <Row>
-                        <Button
-                        href="#home"
-                        >
-                        Home
-                        </Button>
-                    </Row>
-                    <Row>
-                        <Content />
-                    </Row>
-                    <Row>
-                        <Image />
-                    </Row>
-                    <Row>
-                        <Answers />
-                    </Row>
-            <Container>
-                <Row>
-                    <Button
-                    href="#lasttpage"
-                    >
-                    Last Page
-                    </Button>
-                    <Button
-                    href="#nexttpage"
-                    >
-                    Next Page
-                    </Button>
-                </Row>
-            </Container>
+        <div className="story-page">
+            <h2>{story.title}</h2> 
+            <Col size="md-4">
+            <Content>
+              <div id="pageOne">
+              <h3>{story.page1}</h3>
+              <img src={"./images/"+story.page1Keyword+".png"} alt="icon"/>
+              <button className="next-btn" onClick={() => increment()}>Next Page</button>
+              </div>
+
+              <div id="pageTwo" className="hidden">
+              <h3 >{story.page2}</h3>
+              <img src={"./images/"+story.page2Keyword+".png"} alt="icon"/>
+              <button className="next-btn" onClick={() => increment2()}>Next Page</button>
+              </div>
+              
+              <div id="pageThree" className="hidden">
+              <h3 >{story.page3}</h3>
+              <img src={"./images/"+story.page3Keyword+".png"} alt="icon"/>
+              <button className="next-btn" onClick={() => increment3()}>Complete Story!</button>
+              </div>
+            </Content>
+            <Answers>
+              <Row>
+              <div id="pageOne">
+                <div>
+                <Col sm>
+                <button className="answer-btn" size="lg">
+                  {story.page1Options}
+                </button>
                 </Col>
-            </Container>
+                <Col sm>
+                <button className="answer-btn" size="lg">
+                  {story.page1Options}
+                </button>
+                </Col>
+                <Col sm>
+                <button className="answer-btn" size="lg">
+                  {story.page1Options}
+                </button>
+                </Col>
+                </div>
+                </div>
+                </Row>
+            </Answers>
+            </Col>
+            
+        
+         
         </div>
-    )
-}
+          
+      );
+};
 
 export default StoryPage;
