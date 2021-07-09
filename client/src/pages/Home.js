9ec8c44a84094bd3ca94cbb74099c75c4bcab7a0
Joey <joewoodnyc@gmail.com>
2021-07-08 22:33:54 -0400

    IMAGES

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -40,7 +40,9 @@ function Home() {
     }
 
     return (
-        <div className="home">
+
+        <div className="home"> 
+        {/* className="home"> */}
 
             <HText>
               Just Read It!
@@ -49,13 +51,13 @@ function Home() {
                   {stories.length ? (
                 <List>
                   {stories.map(stories => (
-                    <ListItem  key={stories._id} onClick={() => upDateBlurb(stories.synopsis)}>
+                    <ListItem  className="book" key={stories._id} onClick={() => upDateBlurb(stories.synopsis)}>
                         <audio id="pop" src={"./audio/"+"pop"+".mp3"}></audio>
                         <h3 className="story-title">
                           Title: {stories.title} <br></br>
                         </h3>
-                        <img onMouseOver={() => upDateBlurb(stories.synopsis)} onClick={() =>goToBook(stories._id)} href={"/"+stories._id} src="./images/book.png" alt="book"/>
-                        
+                        <img className="book" onMouseOver={() => upDateBlurb(stories.synopsis)} onClick={() =>goToBook(stories._id)} href={"/"+stories._id} src="./images/book.png" alt="book"/>
+
                     </ListItem>
                   ))}
                 </List>
@@ -66,9 +68,31 @@ function Home() {
               <h2 id="Blurb">Hello! Welcome to JustReadIt! Start by clicking on a story to hear about it.</h2>
 
             </Wrapper>
-          
-          
-            
+
+          <div className="background">
+            <div>
+                {/* KIDS */}
+                <img className="kid1" src="./images/background/KID1relative.png" alt="kid1"></img>
+                <img className="kid2" src="./images/background/KID2relative.png" alt="kid2"></img>
+                <img className="kid3" src="./images/background/kid3relative.png" alt="kid3"></img>
+                <img className="kid4" src="./images/background/kid4relative.png" alt="kid4"></img>
+                <img className="kid5" src="./images/background/kid5relative.png" alt="kid5"></img>
+                
+                {/* SHADOWS */}
+                <image className="shadow1" src="./background/SHADOW 1.png"></image>
+                <image className="shadow2" src="./background/SHADOW 2.png"></image>
+                <image className="shadow3" src="./background/SHADOW 3.png"></image>
+                <image className="shadow4" src="./background/SHADOW 4.png"></image>
+                <image className="shadow5" src="./background/SHADOW 5.png"></image>
+
+                {/* SUN AND CLOUDS */}
+                <img className="sun" src="./images/background/SUN.png" alt="sun"></img>
+                <image className="cloud1" src="./background/CLOUD 1.png"></image>
+                <image className="cloud2" src="./background/CLOUD 2.png"></image>
+                
+            </div>
+          </div>
+
         </div>
     )
 
