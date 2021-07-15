3a1c3b1baa9b57fada31f59a1535b12fa487bfcd
Joey <joewoodnyc@gmail.com>
2021-07-14 20:02:35 -0400

    all audio works no volume control homepage

--- client/src/pages/StoryPages/index.js
+++ client/src/pages/StoryPages/index.js
@@ -50,42 +50,120 @@ function StoryRoute(props) {
     }
 	}
 
+  //joey sound stuff
+  function tryAgain() {
+    console.log("tryagain working");
+    var tryAgain = document.getElementById('tryAgain');
+      tryAgain.currentTime = 0;
+      tryAgain.play(); 
+  }
+  function answerCorrect() {
+    console.log("answer working");
+    var answerCorrect = document.getElementById('answerCorrect');
+      answerCorrect.currentTime = 0;
+      answerCorrect.play(); 
+  }
+
+  function answerCorrect2() {
+    console.log("answer working");
+    var answerCorrect = document.getElementById('answerCorrect2');
+      answerCorrect.currentTime = 0;
+      answerCorrect.play(); 
+  }
+
+  function answerCorrect3() {
+    console.log("answer working");
+    var answerCorrect = document.getElementById('answerCorrect3');
+      answerCorrect.currentTime = 0;
+      answerCorrect.play(); 
+  }
+
+  function answerCorrect4() {
+    console.log("answer working");
+    var answerCorrect = document.getElementById('answerCorrect4');
+      answerCorrect.currentTime = 0;
+      answerCorrect.play(); 
+  }
+
+  function storyPrompt() {
+    console.log("storyprompt working");
+    var storyPrompt = document.getElementById('storyPrompt');
+      storyPrompt.currentTime = 0;
+      storyPrompt.play(); 
+  }
+
+  function storyPrompt2() {
+    console.log("storyprompt working");
+    var storyPrompt = document.getElementById('storyPrompt2');
+      storyPrompt.currentTime = 0;
+      storyPrompt.play(); 
+  }
+
+  function storyPrompt3() {
+    console.log("storyprompt working");
+    var storyPrompt = document.getElementById('storyPrompt3');
+      storyPrompt.currentTime = 0;
+      storyPrompt.play(); 
+  }
+
+  function storyPrompt4() {
+    console.log("storyprompt working");
+    var storyPrompt = document.getElementById('storyPrompt4');
+      storyPrompt.currentTime = 0;
+      storyPrompt.play(); 
+  }
+  //end sound stuff
 
   function btn1Press(e) {
     console.log(e.target.value)
     let btnValue=e.target.value;
     if(btnValue === story.page1Keyword) {
-
+      console.log("success")
+      
+      answerCorrect();
       document.getElementById("nextbtn1").classList.remove("hidden")
     } else {
-      window.alert("Try again!")
+      
+      tryAgain();
     }
   }
   function btn2Press(e) {
     console.log(e.target.value)
     let btnValue=e.target.value;
     if(btnValue === story.page2Keyword) {
+      console.log("success")
+      
+      answerCorrect2();
       document.getElementById("nextbtn2").classList.remove("hidden")
     } else {
-      window.alert("try again!")
+      
+      tryAgain();
     }
   }
   function btn3Press(e) {
     console.log(e.target.value)
     let btnValue=e.target.value;
     if(btnValue === story.page3Keyword) {
+      console.log("success")
+      
+      answerCorrect3();
       document.getElementById("nextbtn3").classList.remove("hidden")
     } else {
-      window.alert("try again!")
+      
+      tryAgain();
     }
   }
   function btn4Press(e) {
     console.log(e.target.value)
     let btnValue=e.target.value;
     if(btnValue === story.page4Keyword) {
+      console.log("success")
+      answerCorrect4()
+      
       document.getElementById("nextbtn4").classList.remove("hidden")
     } else {
-      window.alert("try again!")
+      
+      tryAgain();
     }
   }
 
@@ -96,9 +174,14 @@ function StoryRoute(props) {
              <div id="pageOne">
                   <Content >
                   
-                  <h3>{story.page1}</h3>
-                  <img src="./images/sound.png" alt="sound"/>
+                  <h3><img onClick={() => storyPrompt()} id="soundBtn" src="http://localhost:3000/images/sound.png"/>{story.page1}</h3>
+                    
                   <img src={"http://localhost:3000/images/"+story.page1Keyword+".png"} alt="icon"/>
+
+                  <audio id="storyPrompt" src={"http://localhost:3000/audio/"+story.page1Keyword+".mp3"} alt="audio"></audio>
+                  <audio id="answerCorrect" src={"http://localhost:3000/audio/"+story.page1Keyword+"correct.mp3"} alt="audio"></audio>
+                  <audio id="tryAgain" src={"http://localhost:3000/audio/tryagain.mp3"} alt="audio"></audio>
+                  
 
                   {story.page1options ? (
                                         
@@ -125,8 +208,13 @@ function StoryRoute(props) {
                 
               <div id="pageTwo" className="hidden">
               <Content >
-                  <h3 >{story.page2}</h3>
+                  <h3><img onClick={() => storyPrompt2()} id="soundBtn" src="http://localhost:3000/images/sound.png"/>{story.page2}</h3>
                   <img src={"http://localhost:3000/images/"+story.page2Keyword+".png"} alt="icon"/>
+
+                  <audio id="storyPrompt2" src={"http://localhost:3000/audio/"+story.page2Keyword+".mp3"} alt="audio"></audio>
+                  <audio id="answerCorrect2" src={"http://localhost:3000/audio/"+story.page2Keyword+"correct.mp3"} alt="audio"></audio>
+                  <audio id="tryAgain" src={"http://localhost:3000/audio/tryagain.mp3"} alt="audio"></audio>
+
                   {story.page2options ? (
                                         
                       <div>
@@ -151,8 +239,13 @@ function StoryRoute(props) {
               
               <div id="pageThree" className="hidden">
               <Content >
-                  <h3 >{story.page3}</h3>
+                  <h3><img onClick={() => storyPrompt3()} id="soundBtn" src="http://localhost:3000/images/sound.png"/>{story.page3}</h3>
                   <img src={"http://localhost:3000/images/"+story.page3Keyword+".png"} alt="icon"/>
+
+                  <audio id="storyPrompt3" src={"http://localhost:3000/audio/"+story.page3Keyword+".mp3"} alt="audio"></audio>
+                  <audio id="answerCorrect3" src={"http://localhost:3000/audio/"+story.page3Keyword+"correct.mp3"} alt="audio"></audio>
+                  <audio id="tryAgain" src={"http://localhost:3000/audio/tryagain.mp3"} alt="audio"></audio>
+
                   {story.page3options ? (
                                         
                       <div>
@@ -174,8 +267,13 @@ function StoryRoute(props) {
 
               <div id="pageFour" className="hidden">
               <Content >
-                <h3 >{story.page4}</h3>
+                <h3><img onClick={() => storyPrompt4()} id="soundBtn" src="http://localhost:3000/images/sound.png"/>{story.page4}</h3>
                 <img src={"http://localhost:3000/images/"+story.page4Keyword+".png"} alt="icon"/>
+
+                <audio id="storyPrompt4" src={"http://localhost:3000/audio/"+story.page4Keyword+".mp3"} alt="audio"></audio>
+                <audio id="answerCorrect4" src={"http://localhost:3000/audio/"+story.page4Keyword+"correct.mp3"} alt="audio"></audio>
+                <audio id="tryAgain" src={"http://localhost:3000/audio/tryagain.mp3"} alt="audio"></audio>
+
                 {story.page4options ? (
                                       
                     <div>
